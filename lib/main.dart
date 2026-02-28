479731a547e8cae948bbff0319cb612278c4fcf1
Nowshin Fabiha Ibnat <ibnatnstu@gmail.com>
2026-02-28 10:45:49 +0600

    add custom theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_code_base/app.dart';
+import 'package:flutter_code_base/core/services/storage_service.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-void main() {
-  runApp(
-    // Wrap the entire app with ProviderScope
-    const ProviderScope(child: MyApp()),
-  );
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await StorageService.init();
+  runApp(const ProviderScope(child: MyApp()));
 }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp.router(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//       ),
-//       routerConfig: AppRoutes.router,
-//     );
-//   }
-// }
